56fa8bcf61a8c9ac88b22ba58768094587460955
vasichkin <yalov.vasiliy@gmail.com>
2017-12-03 19:05:12 +0300

    ++

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 require_once 'data.php';
 require_once 'functions.php';
 
-$tplContent = getTplContent('index',[
+$tplContent = getTplContent('index.php',[
     "categorys" => $categorys,
     "lots"      => $lots
 ]);
 
-$mainContent = getTplContent('layout', [
+$mainContent = getTplContent('layout.php', [
     "title"         => "YetiCave::Главная",
     "is_auth"       => $is_auth,
     "user_avatar"   => $user_avatar,
